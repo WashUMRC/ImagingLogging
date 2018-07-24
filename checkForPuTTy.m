5077d0866aecde413429f1d86e71cf5c7cdc6a52
WashUMRC <40181349+WashUMRC@users.noreply.github.com>
2018-07-24 09:54:22 -0500

    Update checkForPuTTy.m

--- checkForPuTTy.m
+++ checkForPuTTy.m
@@ -1,7 +1,7 @@
 function checkForPuTTy()
 
-a = system('C:\Program Files\PuTTY\plink.exe')
-if a == 1
+a = exist('C:\Program Files\PuTTY\plink.exe')
+if a ~= 2
   oFN = websave('putty.msi','https://the.earth.li/~sgtatham/putty/latest/w64/putty-64bit-0.70-installer.msi');
   system(oFN);
   system(['del ' oFN]);
